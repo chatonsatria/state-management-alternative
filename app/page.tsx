e8732edf5187d5ebb50a4fb7e567624f04bd368d
chatonsatria <chaton.arkademi@gmail.com>
2024-06-27 22:44:56 +0700

    add button width

--- app/page.tsx
+++ app/page.tsx
@@ -13,17 +13,17 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-6">
       <Link href="/jotai">
-        <button className="px-4 py-2 rounded border bg-blue-600 text-white">
+        <button className="px-4 py-2 w-[100px] rounded border bg-blue-600 text-white">
           Jotai
         </button>
       </Link>
       <Link href="/zustand">
-        <button className="px-4 py-2 rounded border bg-blue-600 text-white">
+        <button className="px-4 py-2 w-[100px] rounded border bg-blue-600 text-white">
           Zustand
         </button>
       </Link>
       <Link href="/recoil">
-        <button className="px-4 py-2 rounded border bg-blue-600 text-white">
+        <button className="px-4 py-2 w-[100px] rounded border bg-blue-600 text-white">
           Recoil
         </button>
       </Link>
